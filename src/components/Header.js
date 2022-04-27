f9007f306e150e1f29de7b02706a6a74323c9f07
IgboPharaoh <solomoneze62@gmail.com>
2022-04-27 01:51:54 +0100

    setup context with reducer functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,11 +7,18 @@ import {
   Nav,
   Navbar,
   NavbarBrand,
+  Button,
 } from "react-bootstrap";
-import {FaShoppingCart} from 'react-icons/fa'
+import { FaShoppingCart } from "react-icons/fa";
+import { AiFillDelete } from "react-icons/ai";
 import { Link } from "react-router-dom";
+import { CartState } from "../context/Context";
 
 const Header = () => {
+  const {
+    state: { cart },
+    dispatch,
+  } = CartState();
   return (
     <Navbar bg="dark" variant="dark" style={{ height: 80 }}>
       <Container>
@@ -26,13 +33,46 @@ const Header = () => {
           />
         </Navbar.Text>
         <Nav>
-          <Dropdown >
+          <Dropdown alignLeft >
             <Dropdown.Toggle variant="success">
               <FaShoppingCart color="white" fontSize="25px" />
-              <Badge bg=''>{10}</Badge>
+              <Badge bg="">{cart.length}</Badge>
             </Dropdown.Toggle>
-            <Dropdown.Menu style={{minWidth: 370}} >
-              <span style={{ padding: 10 }}>Cart is Empty</span>
+            <Dropdown.Menu style={{ minWidth: 370 }} >
+              {cart.length > 0 ? (
+                <>
+                  {cart.map((prod) => (
+                    <span className="cartitem" key={prod.id}>
+                      <img
+                        src={prod.image}
+                        alt={prod.name}
+                        className="cartItemImg"
+                      />
+                      <div className="cartItemDetail">
+                        <span>{prod.name}</span>
+                        <span>{prod.price.split(".")[0]}</span>
+                      </div>
+                      <AiFillDelete
+                        fontSize="20px"
+                        style={{ cursor: "pointer" }}
+                        onClick={() =>
+                          dispatch({
+                            type: "REMOVE_FROM_CART",
+                            payload: prod,
+                          })
+                        }
+                      />
+                    </span>
+                  ))}
+                  <Link to="/cart">
+                    <Button style={{ width: "95%", margin: "0 10px" }}>
+                      Go To Cart
+                    </Button>
+                  </Link>
+                </>
+              ) : (
+                <span style={{ padding: 10 }}>Cart is Empty</span>
+              )}
             </Dropdown.Menu>
           </Dropdown>
         </Nav>
--- src/components/SingleProduct.js
+++ src/components/SingleProduct.js
@@ -1,17 +1,22 @@
 import React from "react";
 import { Button, Card } from "react-bootstrap";
+import { CartState } from "../context/Context";
 import Rating from "./Rating";
 import "./styles.css";
 
 const SingleProduct = ({ prod }) => {
+  const {
+    state: { cart },
+    dispatch,
+  } = CartState();
   return (
     <div className="products">
       <Card>
         <Card.Img variant="top" src={prod.image} alt={prod.name} />
         <Card.Body>
           <Card.Title>{prod.name}</Card.Title>
-          <Card.Subtitle>
-            <span style={{ paddingBottom: 10 }}>
+          <Card.Subtitle style={{ paddingBottom: 10 }} >
+            <span >
               ${prod.price.split(".")[0]}
               {prod.fastDelivery ? (
                 <div>Fast Delivery</div>
@@ -21,8 +26,26 @@ const SingleProduct = ({ prod }) => {
             </span>
             <Rating rating={prod.rating} />
           </Card.Subtitle>
-          <Button variant="danger">Remove From Cart</Button>
-          <Button disabled={!prod.inStock} >{ !prod.inStock ? "Out of Stock" : "Add To Cart"}</Button>
+          {cart.some((x) => x.id === prod.id) ? (
+            <Button variant="danger" onClick={() => {
+                dispatch({
+                  type: "REMOVE_FROM_CART",
+                  payload: prod,
+                });
+              }}>Remove From Cart</Button>
+          ) : (
+            <Button
+              disabled={!prod.inStock}
+              onClick={() => {
+                dispatch({
+                  type: "ADD_TO_CART",
+                  payload: prod,
+                });
+              }}
+            >
+              {!prod.inStock ? "Out of Stock" : "Add To Cart"}
+            </Button>
+          )}
         </Card.Body>
       </Card>
     </div>
